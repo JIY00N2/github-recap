230def786a2e6b6e43ab12319f6b0982bab72576
JIY00N2 <angella990825@gmail.com>
2024-04-29 23:36:46 +0900

    feat: github client id, secret env 추가 (#3)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   env: {
     GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
   },
 };
 

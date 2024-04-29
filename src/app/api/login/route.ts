78981fd734ea962b0e1873df7505560aa66a89d3
JIY00N2 <angella990825@gmail.com>
2024-04-30 00:04:42 +0900

    feat: 로그인 route handler 에러 처리 (#3)

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -7,16 +7,26 @@ export async function POST(request: NextRequest, response: NextResponse) {
   const provider = searchParams.get("provider");
 
   if (!code || !provider) {
-    return NextResponse.error();
+    return NextResponse.json(
+      { message: "code와 provider가 필요합니다." },
+      { status: 400 },
+    );
   }
 
   let accessToken: string | null = null;
+  let errorMessage: string | null = null;
 
   // Content-type: server가 받는 data type json일 때,
   // Accept: 내가 응답을 json 형식으로 받겠다..
   // next server: client, github server: server
   if (provider === "github") {
-    accessToken = (await githubLogin(code)).access_token;
+    const data = await githubLogin(code);
+    accessToken = data.access_token;
+    errorMessage = data.error_description;
+  }
+
+  if (errorMessage) {
+    return NextResponse.json({ message: errorMessage }, { status: 500 });
   }
 
   if (!accessToken) {
